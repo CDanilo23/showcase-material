64532d245b6a81d44634f621b22533fb7f46d04c
Cristian Danilo Ordoñez <cristian.ordonez@globant.com>
2018-09-24 17:20:31 -0500

    se reestablese showcase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PagesModule } from './pages/pages.module';
+import { APP_ROUTES } from './app.routes';
 
 import { AppComponent } from './app.component';
 
-// Modules
-import { PagesModule } from './pages/pages.module';
-import { APP_ROUTES } from './app.routes';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
 @NgModule({
   imports: [
-    BrowserModule,
     PagesModule,
-    FormsModule,
     APP_ROUTES,
-    BrowserAnimationsModule,
-    ReactiveFormsModule,
   ],
   declarations: [
     AppComponent,
